55dc621a940571908ffcb69f264580a33e42b760
Tim <tkolenda@gmail.com>
2019-04-21 12:08:06 -0400

    added some utils as well as styling package

---
+++ src/utils/options.js
@@ -0,0 +1,41 @@
+
+
+const options = [
+    {
+        type: 'rock',
+        img: 'assets/rock.png',
+        alt: 'Image of a hand in a fist, indicating rock!',
+        index: 0,
+        customClass: 'rockClass'
+    },
+    {
+        type: 'paper',
+        img: 'assets/paper.png',
+        alt: 'Image of a flat hand, indicating paper!',
+        index: 1,
+        customClass: 'paperClass'
+    },
+    {
+        type: 'scissors',
+        img: 'assets/scissors.png',
+        alt: 'Image of a hand with two fingers out, indicating scissors!',
+        index: 2,
+        customClass: 'scissorsClass'
+    },
+    {
+        type: 'lizard',
+        img: 'assets/lizard.png',
+        alt: 'Image of a hand forming a mouth, indicating lizard!',
+        index: 3,
+        customClass: 'lizardClass'
+    },
+    {
+        type: 'spock',
+        img: 'assets/spock.png',
+        alt: 'Image of a hand giving the Vulcan salute, indicating spock!',
+        index: 4,
+        customClass: 'spockClass'
+    }
+]
+
+export default options;
