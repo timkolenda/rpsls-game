934496167c3ad545f265a5abb62896a6aac40f34
Tim <tkolenda@gmail.com>
2019-04-21 14:22:24 -0400

    Updates

---
+++ precache-manifest.f5a4b30b277c0ace6f4527c676111555.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbe3845f72a04307de66",
+    "url": "/rpsls-game/static/css/main.1ea41505.chunk.css"
+  },
+  {
+    "revision": "fbe3845f72a04307de66",
+    "url": "/rpsls-game/static/js/main.2458e38a.chunk.js"
+  },
+  {
+    "revision": "4705b773e1cc9c4a83b8",
+    "url": "/rpsls-game/static/js/runtime~main.4cd2ea22.js"
+  },
+  {
+    "revision": "aa8b7cd76d79b0da6e1c",
+    "url": "/rpsls-game/static/js/2.e1022d1a.chunk.js"
+  },
+  {
+    "revision": "ce8f19bd627e77ba0995574f91143385",
+    "url": "/rpsls-game/static/media/background.ce8f19bd.png"
+  },
+  {
+    "revision": "bb089555380a452dd2021e1fc234f32a",
+    "url": "/rpsls-game/static/media/game-display-desktop.bb089555.png"
+  },
+  {
+    "revision": "84b86c0134636d365a3939b3eddb13f2",
+    "url": "/rpsls-game/static/media/rules.84b86c01.png"
+  },
+  {
+    "revision": "848cffc557837977403817c4fe339d5a",
+    "url": "/rpsls-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/test-deploy/precache-manifest.64dd75290c382b27ada39b2a51aa57a2.js"
+  "/rpsls-game/precache-manifest.f5a4b30b277c0ace6f4527c676111555.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/test-deploy/index.html", {
+workbox.routing.registerNavigationRoute("/rpsls-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
